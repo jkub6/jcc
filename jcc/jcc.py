eb10d003b0fd17b5809b2cb00acc03b06d75eb6e
Jake <jkub66@gmail.com>
2019-09-11 12:09:03 -0600

    setup pyinstaller

--- jcc/jcc.py
+++ jcc/jcc.py
@@ -6,10 +6,29 @@ This file is a part of Jake's C Compiler (JCC)
 """
 
 import argparse
+import jcc.token
 
-parser = argparse.ArgumentParser()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
 
-parser.add_argument('file', metavar='file',
-                    help='input file to be compiled')
+    parser.add_argument('file', metavar='file',
+                        help='input file to be compiled')
 
-args = parser.parse_args()
+    args = parser.parse_args()
+
+    filename = args.file
+
+    print("loading file...\n")
+
+    file = open(filename, "r")
+
+    source = file.read()
+
+    print(source)
+
+    tokens = jcc.token.tokenize(source)
+
+    for token in tokens:
+        print(token)
+
+    input("Press enter to exit...")
